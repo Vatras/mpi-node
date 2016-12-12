f420c4b26c05ac5a552c7dc3424669c70dc288be
pjesek <fabisiak.tomek@gmail.com>
2016-12-12 11:45:12 +0100

    portsMap

--- app.js
+++ app.js
@@ -5,9 +5,34 @@ var stdio = require('stdio');
 var ops = stdio.getopt({
     'numOfProcesses': {key: 'n',args: 1, mandatory: true,description: 'Another description'}
 });
-portfinder.basePort=12110;
-console.log("ops[numOfProcesses]"+ops[numOfProcesses]);
 
-portfinder.getPort(function (err, port) {
-console.log(port);
+function getPorts(processesNumber){
+    var deferred = Q.defer();
+    var portsArray=[];
+    for(var i=0;i<numOfProcesses;i++){
+        portfinder.getPort(function (err, port) {
+            portsArray.push(port);
+            console.log(port);
+            if(portsArray.length==processesNumber){
+                deferred.resolve(portsArray);
+            }
+        });
+    }
+    return deferred.promise;
+}
+
+portfinder.basePort=12110;
+var numOfProcesses = ops['numOfProcesses'];
+
+getPorts(numOfProcesses).then(function(data){
+    console.log("portsArray= "+data);
+    var portsMap = {};
+    var processMap = data.map(function(item,index){
+        portsMap[index]=item;
+    })
+    console.log(portsMap);
 });
+
+
+
+
