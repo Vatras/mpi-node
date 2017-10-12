7041ec4c922006e75f063c12666fc357cdf33ca6
tomekf <tomasz.fabisiak@br-ag.eu>
2017-10-12 20:15:17 +0200

    minor patch

--- mpi.js
+++ mpi.js
@@ -1,4 +1,5 @@
 var portfinder = require('portfinder');
+portfinder.basePort=12110;
 var Q = require('q');
 var fs = require('fs');
 var child_process = require('child_process');
@@ -6,15 +7,18 @@ var stdio = require('stdio');
 var net = require('net');
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
+var CodeDistribution = require('./lib/codeDistribution')
 var JsonSocket = require('json-socket')
 var ops = stdio.getopt({
     'processToCreate': {key: 'n',args: 1, description: 'Number of processes given to initiator to create (including itself)'},
+    'port': {key: 'r',args: 1, description: 'Port to use'},
     'numOfProcesses': {key: 'p',args: 1, description: 'Number of processes to differ initiator from child processes'},
     'tid': {key: 't',args: 1,description: 'Id of process'},
-    'initiatorPort': {key: 'a',args: 1,description: 'Id of process'}
+    'initiatorPort': {key: 'a',args: 1,description: 'Id of process'},
+    'initiatorHostname': {key: 'h',args: 1,description: 'Hostname of initiator'}
 });
+var ip = require("ip");
 var fs = require('fs');
-portfinder.basePort=12110;
 var numOfProcesses = ops['processToCreate'] ? parseInt(ops['processToCreate']) : parseInt(ops['numOfProcesses']);
 var isInitiator = ops['processToCreate'] ? true : false;
 var tid = isInitiator ? 0 : parseInt(ops['tid']);
@@ -24,15 +28,6 @@ var socketMap=[];
 var initiatorServer;
 var server;
 var debugTid;
-function writePortsToFile(ports){
-    fs.writeFile("public/ports.json", JSON.stringify(ports), function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-    }); 
-    
-}
 function connect(host,port){
     var socket = new JsonSocket(new net.Socket()); //Decorate a standard net.Socket with JsonSocket
     socket.connect(port, host);
@@ -41,88 +36,106 @@ function connect(host,port){
 
 function getPorts(processesNumber){
     var deferred = Q.defer();
-    var portsArray=[];
-    for(var i=0;i<numOfProcesses;i++){
-        portfinder.getPort(function (err, port) {
-            portsArray.push(port);
-            console.log(port);
-            if(portsArray.length==processesNumber){
-                deferred.resolve(portsArray);
-            }
-        });
-    }
+    portfinder.getPorts(parseInt(numOfProcesses),{},function (err, portsArray) {
+        if(portsArray.length==processesNumber){
+            deferred.resolve(portsArray);
+        }
+    });
     return deferred.promise;
 }
 
 
-function createProcesses(){
-    for(var i = 1; i<numOfProcesses; i++) {
+var myIp = ip.address();
+function createProcesses(processesToCreate){
+    for(var i = 1; i<processesToCreate; i++) {
         var debug='';
         if(debugTid === i){
             debug='--debug '
         }
         console.log(portsMap);
-        child_process.exec('node '+debug+'app.js'+'>tid_'+i+' -t '+i+' -a '+myPort+' -p '+numOfProcesses);
+        child_process.exec('node '+debug+'app.js'+'>tid_'+i
+            +' -t '+i+
+            ' -a '+myPort+
+            ' -h '+myIp+
+            ' -p '+processesToCreate+
+            ' -r '+portsMap[i]+
+            ' 2>err_tid'+i);
         console.log(i);
     }
 
 }
+var initiatorPort;
+var globalTid;
+var initiatorHostname;
+var hostsList;
 function init(cb,tidToDebug){
     setInterval(function(){},1000000);
     debugTid = tidToDebug;
     if(isInitiator){
         getPorts(numOfProcesses).then(function(data){
+            console.log(ip.address());
             console.log("portsArray= "+data);
             portsMap=data;
             console.log("writing",portsMap)
-            writePortsToFile(portsMap)
             myPort=data[0];
-            createServer(cb).then(createProcesses());
+            hostsList = CodeDistribution.distributeCodeToHosts(myPort,ip.address(),numOfProcesses);
+            var local=hostsList.hosts.find(function(host){
+                return host.hostname=='localhost'
+            })
+            var localProcesses = local ? local.processes : 0;
+            createServer(cb).then(createProcesses(localProcesses));
         });
     }
     else{
+        myPort=ops['port'];
         console.log("child process. Tid="+tid);
-        var initiatorPort = ops['initiatorPort'];
+        initiatorPort = ops['initiatorPort'];
+        initiatorHostname = ops['initiatorHostname'];
         console.log("initiatorPort: "+initiatorPort);
-        createSocketConnectionWithInitiator(initiatorPort,cb);
+        console.log("initiatorHost: "+initiatorHostname);
+        createSocketConnectionWithInitiator(cb);
     }
 }
 
 
-function createSocketConnectionWithInitiator(initiatorPort,cb){
+function createSocketConnectionWithInitiator(cb){
     var deferred = Q.defer();
-    initiatorServer = connect('localhost',initiatorPort);
+    initiatorServer = connect(initiatorHostname,initiatorPort);
     function createSocketConnection(){
         var numberOfConnections=1;
+        numOfProcesses=portsMap.length;
         socketMap =  new Array(numOfProcesses);
-         portsMap.forEach(function(port,index){
+         portsMap.forEach(function(hostData,index){
+             var port = hostData.port;
+             var hostname = hostData.host;
             if(index==0){
                 socketMap[index] = initiatorServer;
                 return;
             }
-            var tempSocket = connect('localhost',port);
+            var tempSocket = connect(hostname,port);
             tempSocket.on('connect', function() {
+                console.log('connected', numberOfConnections, numOfProcesses)
                 socketMap[index] = tempSocket;
-                tempSocket.sendMessage({type: 'initial', content:tid})
+                tempSocket.sendMessage({type: 'initial', content:globalTid})
                 numberOfConnections++;
                 if(numberOfConnections==numOfProcesses){
-                  deferred.resolve();
+                    console.log('resolve');
+                    deferred.resolve();
                 }
             });
         })
         return deferred.promise;
     }
     initiatorServer.on('connect', function() { //Don't send until we're connected
-        initiatorServer.on('message', function(message) {
-            if(message.type === 'portsMap'){
-            portsMap = message.array;
-            console.log('Client received'+portsMap);
-            myPort = portsMap[tid];
-            createServerForClients()
-                .then(createSocketConnection)
-                .then(cb);
-            }
-        });
+        // initiatorServer.emit('myPort',{myPort:myPort})
+        initiatorServer.sendMessage({
+            type: 'myPort',
+            port:myPort,
+            host: myIp
+        })
+        createServerForClients()
+            .then(createSocketConnection)
+            .then(cb);
     });
 
 }
@@ -137,17 +150,15 @@ function createServer(cb){
     });
 
     var creationsNumber=1;
+    var connectionCount=1;
+    globalTid=0;
+    var portsMap=[{tid:0,port:myPort,host:myIp}];
     server.on('connection', function(socket) { //This is a standard net.Socket
         console.log("connection from client");
         socket = new JsonSocket(socket); //Now we've decorated the net.Socket to be a JsonSocket
 
-        socket.sendMessage({type: 'portsMap',array: portsMap})
-
         socket.on('message', function(message,a,b) {
             if(message.type === 'serverCreated'){
-                console.log("server received: "+JSON.stringify(message));
-                socket.processTid = message.tid
-                socketMap.push({tid:message.tid, socket: socket})
                 creationsNumber++;
                 if(creationsNumber === numOfProcesses){
                     socketMap=socketMap.sort(function(a,b){
@@ -156,9 +167,17 @@ function createServer(cb){
                     socketMap=socketMap.map(function(item,index){
                         return item.socket;
                     })
-                    broadcast({type:'allServersCreated'});
+                    broadcast({type:'allServersCreated',portsMap:portsMap});
                     cb();
                 }
+            }
+            else if(message.type === 'myPort'){
+                var nodeGlobalTid = connectionCount;
+                portsMap.push({tid: nodeGlobalTid,port:message.port,host:message.host});
+                socketMap.push({tid:nodeGlobalTid, socket: socket})
+                socket.processTid = nodeGlobalTid
+                socket.sendMessage({type: 'globalTid',globalTid: nodeGlobalTid})
+                connectionCount++;
             }
             else{
                 message.processTid = socket.processTid;
@@ -171,7 +190,7 @@ function createServer(cb){
 function broadcast(message){
     console.log("broadcasting",JSON.stringify(message));
     socketMap.forEach(function(socket,index){
-        if(index!=tid){
+        if(index!=globalTid){
             socket.sendMessage(message);
         }
     })
@@ -185,12 +204,17 @@ function createServerForClients(){
     var deferred = Q.defer();
     server = net.createServer();
     server.listen({port: myPort},function(){
-        initiatorServer.sendMessage({tid: tid,type: 'serverCreated'})
+        initiatorServer.sendMessage({type: 'serverCreated'})
         initiatorServer.on('message',function(message){
+            if(message.type=='globalTid'){
+                globalTid=message.globalTid
+                console.log('assigned global tid:',globalTid)
+            }
             console.log('initiator sent:',JSON.stringify(message));
             message.processTid=0;
             eventEmitter.emit(message.type,message);
             if(message.type==='allServersCreated'){
+                portsMap=message.portsMap
                 console.log('received allServersCreated')
                 //initiatorServer.removeListener(startFunction);
                 deferred.resolve();
@@ -228,12 +252,15 @@ function eventEmitterOn(event,cb){
 function getPort(){
     return myPort
 }
+function getGlobalTid(){
+    return globalTid
+}
 module.exports = {
     init: init,
     broadcast:broadcast,
     send : send,
     recv : eventEmitterOn,
-    rank : tid,
+    rank : getGlobalTid,
     myPort : getPort,
     size : numOfProcesses
 }
